1929f79ca6128e5a597550f9fac1faf04705b6b5
Sarthak <sarthakss898@gmail.com>
2025-05-12 20:10:32 +0530

    Fix layout shifting due to alert

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({ message, type });
+    setTimeout(() => setAlert(null), 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <Router basename="/TextUtils">
+      {/* Shared UI */}
+      <Navbar
+        title="TextUtils"
+        aboutText="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <div style={{height:'50px'}}>
+        <Alert alert={alert} />
+      </div>
+
+      <div className="container my-3">
+        <Routes>
+          {/* Route-specific content */}
+          <Route
+            path="/"
+            element={
+              <Textform
+                heading="Enter text to analyze below"
+                mode={mode}
+                showAlert={showAlert}
+              />
+            }
+          />
+          <Route path="/about" element={<About mode={mode} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+
+export default function Textform(props) {
+  const handleUpClick = () => {
+    let newtext = text.toUpperCase();
+    setText(newtext);
+  };
+
+  const handleLowClick = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+
+    const handleExtraSpaces = () => {
+    let words = text.split(" ").filter((word) => word !== "");
+    setText(words.join(" "));
+  };
+
+  const handleOnChange = (event) => {
+    console.log(text)
+    setText(event.target.value);
+  };
+
+  const handleClearClick = () => {
+    setText("");
+  };
+
+  const handleCopy = () => {
+    const textarea = document.getElementById("mybox");
+    textarea.select();
+    navigator.clipboard.writeText(text);
+  };
+
+
+
+  const handleHeadlineClick = () => {
+    let ar = text.split(" ");
+    let sentence = "";
+    ar.forEach((word) => {
+      if (word) {
+        sentence = sentence + word[0].toUpperCase() + word.slice(1).toLowerCase() + " ";
+      }
+    });
+    setText(sentence.slice(0,-1));
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container" style = {{color : props.mode==='dark'?'white':'#042743'}}>
+        <h1 >{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            value={text}
+            className="form-control"
+            id="mybox"
+            rows="8"
+            onChange={handleOnChange}
+            style = {{backgroundColor : props.mode==='light'?'white':'#042743',color : props.mode==='dark'?'white':'#042743'}}
+          ></textarea>
+        </div>
+      </div>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>
+        Convert to UPPERCASE
+      </button>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>
+        Convert to lowercase
+      </button>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleHeadlineClick}>
+        Convert to HeadLine
+      </button>
+      <button className="btn btn-danger mx-2 my-2" onClick={handleClearClick}>
+        Clear text
+      </button>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>
+        Copy text
+      </button>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>
+        Remove extra spaces
+      </button>
+      <div className="container my-2" style = {{color : props.mode==='dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words</p>
+        <p>{text.length} characters</p>
+        <p>Reading time - {(text.split(" ").filter((element)=>{return element.length!==0}).length - 1) / 125} minutes</p>
+        <h2>Preview</h2>
+        <p>{text?text:"Enter some text first"}</p>
+      </div>
+    </>
+  );
+}
